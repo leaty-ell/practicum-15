f97914a774e36a7547e17aac95b6a64dd863ddef
leaty-ell <lisalimanova14@gmail.com>
2025-11-17 10:19:07 +0700

    Create  tsk14.py

---
+++ tsk14.py
@@ -0,0 +1,23 @@
+def numbers(x: int):
+    """
+    Recursive function to print digits of natural number in reverse order.
+    
+    Args:
+        x (int): Natural number
+    """
+    if x < 10:
+        print(x)
+    else:
+        print(x % 10)
+        numbers(x // 10)
+
+
+def main():
+    x = int(input("Введите натуральное число: "))
+    
+    print("Цифры в обратном порядке:")
+    numbers(x)
+
+
+if __name__ == "__main__":
+    main()
